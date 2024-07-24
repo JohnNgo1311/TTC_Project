b9a19c9c8613de1398a282852c57b5a18fc82a74
Ngo Xuan Nghia <JohnNgo1311@gmail.com>
2024-07-24 22:45:06 +0700

    push code 24/7 22:40

---
+++ TTC_AR/Assets/Scripts/DropdownSearch.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI.Extensions;
+
+public class DropdownSearch : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_InputField inputField;
+    public GameObject parentGameObject; // GameObject cha chứa các GameObject mục tiêu
+
+    void Start()
+    {
+        // Đảm bảo rằng inputField không null
+        if (inputField != null)
+        {
+            // Đăng ký sự kiện onValueChanged
+        }
+    }
+
+    public void OnInputValueChanged(string input)
+    {
+        // Vô hiệu hóa tất cả các GameObject con của GameObject cha
+        foreach (Transform child in parentGameObject.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+        // Kiểm tra văn bản của InputField và kích hoạt GameObject tương ứng
+        foreach (Transform child in parentGameObject.transform)
+        {
+            if (child.gameObject.name.ToLower() == input.ToLower())
+            {
+                child.gameObject.SetActive(true);
+                break; // Dừng lại sau khi kích hoạt phần tử khớp
+            }
+        }
+    }
+}
