8e9910ce4c60312d7fb84ebdde2d2c090204bc4e
Ngo Xuan Nghia <JohnNgo1311@gmail.com>
2024-08-30 16:22:03 +0700

    push code 30/8/2024 16:03 - build scroll area prefab cho searchable - Continue

--- TTC_AR/Assets/Scripts/DropdownSearch.cs
+++ TTC_AR/Assets/Scripts/DropdownSearch.cs
@@ -1,44 +1,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 public class DropdownSearch : MonoBehaviour
 {
-    [SerializeField]
+    public GameObject prefab_Device;
     private TMP_InputField inputField;
+
     [SerializeField]
     private GameObject parentGameObject; // GameObject cha chứa các GameObject mục tiêu
 
     void Awake()
+    {
+
+    }
+
+    void start()
     {
         // Kiểm tra nếu `inputField` hoặc `parentGameObject` không được gán
         if (inputField == null || parentGameObject == null)
         {
             Debug.LogError("InputField hoặc ParentGameObject không được gán!");
         }
+        else
+        {
+            inputField.onValueChanged.AddListener(OnInputValueChanged);
+        }
     }
-
-    public void OnInputValueChanged(string input)
+    private void OnInputValueChanged(string input)
     {
         if (string.IsNullOrEmpty(input)) return;
-
-        bool foundMatch = false;
-
-        foreach (Transform child in parentGameObject.transform)
+        else
         {
-            bool isMatch = string.Equals(child.gameObject.name, input, System.StringComparison.OrdinalIgnoreCase);
-            child.gameObject.SetActive(isMatch);
-            if (isMatch)
+            if (prefab_Device != null)
             {
-                foundMatch = true;
-                break; // Dừng lại sau khi kích hoạt phần tử khớp
+                prefab_Device.gameObject.name = "Scroll_Area_01TT007";
             }
+            //! device_Information
+            TMP_Text code_Value_Text = prefab_Device.transform.Find("Content/Device_Information/Code_group/Code_value").GetComponent<TMP_Text>();
+            TMP_Text function_Value_Text = prefab_Device.transform.Find("Content/Device_Information/Function_group/Funtcion_value").GetComponent<TMP_Text>();
+            TMP_Text range_Value_Text = prefab_Device.transform.Find("Content/Device_Information/Range_group/Range_value").GetComponent<TMP_Text>();
+            TMP_Text io_Value_Text = prefab_Device.transform.Find("Content/Device_Information/IO_group/IO_value").GetComponent<TMP_Text>();
+            //! JB_Connection
+            TMP_Text jb_Connection_Value_Text = prefab_Device.transform.Find("Content/JB_Connection_group/JB_Connection_text_group/JB_Connection_Value").GetComponent<TMP_Text>();
+            TMP_Text jb_Connection_Location_Text = prefab_Device.transform.Find("Content/JB_Connection_group/JB_Connection_text_group/JB_Connection_Location").GetComponent<TMP_Text>();
         }
 
-        // Nếu không tìm thấy khớp, có thể thực hiện xử lý bổ sung ở đây
-        if (!foundMatch)
-        {
-            Debug.Log("Không tìm thấy mục nào khớp với đầu vào: " + input);
-        }
     }
 }
